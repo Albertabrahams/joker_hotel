572b3980e126fd53d7c4cc09443012a78e3f1dec
Albertabrahams <halilibrahim.soyman@gmail.com>
2024-03-28 13:26:23 +0300

    settings updated

--- hotel/settings.py
+++ hotel/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['hotel.herokuapp.com',]
+ALLOWED_HOSTS = ['hotel.herokuapp.com', 'https://joker-hotel-front.vercel.app/']
 
 
 # Application definition
